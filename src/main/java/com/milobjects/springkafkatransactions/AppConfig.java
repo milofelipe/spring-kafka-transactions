2fc1b7e5c20b2dee78e049494afaa7260d02d331
Milo Felipe <milo@felipe.ph>
2020-06-08 18:35:17 +1000

    Remove not needed @EnableScheduling annotation

--- src/main/java/com/milobjects/springkafkatransactions/AppConfig.java
+++ src/main/java/com/milobjects/springkafkatransactions/AppConfig.java
@@ -15,7 +15,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import javax.persistence.EntityManagerFactory;
 
 @Configuration
-@EnableScheduling
 public class AppConfig {
 
     @Bean
