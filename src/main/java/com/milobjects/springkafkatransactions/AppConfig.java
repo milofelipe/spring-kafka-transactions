f419d1dc12de024fa3581a29595bbe9466d8056b
Milo Felipe <milo@felipe.ph>
2020-09-22 14:35:02 +1000

    Standalone beans for transactions not started by the listener container. TransactionPrefixIds are overridden to be unique for each instance.

--- src/main/java/com/milobjects/springkafkatransactions/AppConfig.java
+++ src/main/java/com/milobjects/springkafkatransactions/AppConfig.java
@@ -4,14 +4,18 @@ import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.boot.autoconfigure.kafka.ConcurrentKafkaListenerContainerFactoryConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.transaction.ChainedKafkaTransactionManager;
 import org.springframework.kafka.transaction.KafkaTransactionManager;
 import org.springframework.orm.jpa.JpaTransactionManager;
 
 import javax.persistence.EntityManagerFactory;
+import java.util.UUID;
 
 @Configuration
 public class AppConfig {
@@ -37,6 +41,32 @@ public class AppConfig {
         configurer.configure(factory, consumerFactory);
         factory.getContainerProperties().setTransactionManager(chainedKafkaTransactionManager);
         return factory;
+    }
+
+    @Bean
+    @Primary
+    KafkaTemplate<String, String> kafkaTemplate(ProducerFactory producerFactory) {
+        return new KafkaTemplate(producerFactory);
+    }
+
+    @Bean
+    @Primary
+    KafkaTransactionManager<String, String> kafkaTransactionManager(ProducerFactory producerFactory) {
+        return new KafkaTransactionManager<>(producerFactory);
+    }
+
+    @Bean("standaloneKafkaTemplate")
+    KafkaTemplate<String, String> standaloneKafkaTemplate(ProducerFactory producerFactory) {
+        KafkaTemplate<String, String> kafkaTemplate = new KafkaTemplate(producerFactory);
+        kafkaTemplate.setTransactionIdPrefix(String.format("tx-%s-", UUID.randomUUID().toString()));
+        return kafkaTemplate;
+    }
+
+    @Bean("standaloneKafkaTransactionManager")
+    KafkaTransactionManager<String, String> standaloneKafkaTransactionManager(ProducerFactory producerFactory) {
+        KafkaTransactionManager<String, String> kafkaTransactionManager = new KafkaTransactionManager<>(producerFactory);
+        kafkaTransactionManager.setTransactionIdPrefix(String.format("tx-%s-", UUID.randomUUID().toString()));
+        return kafkaTransactionManager;
     }
 
     @Bean
